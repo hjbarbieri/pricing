c6936d042be4c17682ce9c7fbe9e9d06d9216ba6
javierbarbieri <hj.barbieri@gmail.com>
2017-04-06 19:52:26 +0100

    Tests

--- src/test/java/PriceTest.java
+++ src/test/java/PriceTest.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
 /**
  * Created by javierbarbieri on 06/04/2017.
  */
 public class PriceTest {
+
+    @Test
+    public void shouldApplyDiscountToProduct() {
+
+    }
 }
